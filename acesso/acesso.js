28e2f735500326dc1b2bc5a08b369eb44abbe7f9
Henrique rocha <henriquerocha1357@gmail.com>
2023-10-09 21:51:02 -0300

    :inicio initial commit

---
+++ acesso/acesso.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector("#btn");
+
+function send() {
+    const usuario = document.querySelector(".usuario").value;
+    const senha = document.querySelector(".senha").value;
+    const preencha = document.querySelector("#preencha");
+    const sucesso = document.querySelector("#sucesso");
+    const negado = document.querySelector("#negado"); // Adicione esta linha
+
+    if (usuario.length < 1 || senha.length < 1) {
+        preencha.style.display = 'inline';
+
+        setTimeout(() => {
+            preencha.style.display = 'none';
+        }, 1000);
+
+        return false;
+    }
+
+    if (usuario === "roberta" && senha === "10") {
+        sucesso.style.display = 'inline';
+
+        setTimeout(() => {
+            location.href = "http://127.0.0.1:5500/home/home.html";
+        }, 500);
+    } else {
+        negado.style.display = 'inline';
+
+        setTimeout(() => {
+            location.href = "http://127.0.0.1:5500/error/erro.html";
+        }, 500);
+    }
+}
+
+// Adicione um ouvinte de evento de clique ao botão
+btn.addEventListener("click", send);
